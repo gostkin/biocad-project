09a4b2fa6d1abef7b54adbdad2c4e4e1b480529d
Eugene Gostkin <gostkin.em@phystech.edu>
2019-01-28 03:18:26 +0300

    Setup main executables, add Travis CI support

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2,3 +2,6 @@
 // Created by gostkin on 28.01.19.
 //
 
+int main() {
+  return 0;
+}
--- src/test/TestMain.cpp
+++ src/test/TestMain.cpp
@@ -2,3 +2,12 @@
 // Created by gostkin on 28.01.19.
 //
 
+#include <gtest/gtest.h>
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+
+  const int code = RUN_ALL_TESTS();
+  printf("[==========]\r\n\r\nTests execution completed with code: %d \r\n\r\n", code);
+  return code;
+}
